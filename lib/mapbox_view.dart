a32c911cefa88cc73550753a9e2dbb389aae82c8
DiRaizel <morales.bs18@gmail.com>
2022-11-12 12:36:42 -0500

    .

--- lib/mapbox_view.dart
+++ lib/mapbox_view.dart
@@ -15,16 +15,21 @@ class MapboxNavigationView extends StatelessWidget {
   MapboxOptions mapboxOptions;
 
   // New Event Channel
-  static const EventChannel _mapboxViewChannel = EventChannel('mapboxView/events');
+  static const EventChannel _mapboxViewChannel =
+      EventChannel('mapboxView/events');
 
   final _navigationWithMapboxPlugin = NavigationWithMapbox();
 
   starNavigation() async {
     await _navigationWithMapboxPlugin.startNavigation(
         // origin refers to the user's starting point at the time of starting the navigation
-        origin: WayPoint(latitude: mapboxOptions.origin.latitude, longitude: mapboxOptions.origin.longitude),
+        origin: WayPoint(
+            latitude: mapboxOptions.origin.latitude,
+            longitude: mapboxOptions.origin.longitude),
         // destination refers to the end point or goal for the user at the time of starting the navigation
-        destination: WayPoint(latitude: mapboxOptions.destination.latitude, longitude: mapboxOptions.destination.longitude),
+        destination: WayPoint(
+            latitude: mapboxOptions.destination.latitude,
+            longitude: mapboxOptions.destination.longitude),
         // if we enable this option we can choose a destination with a sustained tap
         setDestinationWithLongTap: mapboxOptions.setDestinationWithLongTap,
         // if we enable this option we will activate the simulation of the route
@@ -33,13 +38,17 @@ class MapboxNavigationView extends StatelessWidget {
         msg: (mapboxOptions.msg != null) ? mapboxOptions.msg : '',
         // unit of measure in which the navigation assistant will speak to us
         // optional, default: metric
-        voiceUnits: (mapboxOptions.voiceUnits != null) ? mapboxOptions.voiceUnits : '',
+        voiceUnits:
+            (mapboxOptions.voiceUnits != null) ? mapboxOptions.voiceUnits : '',
         // language in which the navigation assistant will speak to us
         // optional, default: en
-        language: (mapboxOptions.language != null) ? mapboxOptions.language : '',
+        language:
+            (mapboxOptions.language != null) ? mapboxOptions.language : '',
         // if we enable this option we can see alternative routes when starting the navigation map
         // optional, default: false
-        alternativeRoute: (mapboxOptions.alternativeRoute != null) ? mapboxOptions.alternativeRoute : false,
+        alternativeRoute: (mapboxOptions.alternativeRoute != null)
+            ? mapboxOptions.alternativeRoute
+            : false,
         // the style or theme with which the navigation map will be loaded
         // optional, default: streets, others: dark, light, traffic_day, traffic_night, satellite, satellite_streets, outdoors
         style: (mapboxOptions.style != null) ? mapboxOptions.style : '',
@@ -51,15 +60,23 @@ class MapboxNavigationView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Map data = {
-      'destination': {'Latitude': mapboxOptions.destination.latitude, 'Longitude': mapboxOptions.destination.longitude},
-      'origin': {'Latitude': mapboxOptions.origin.latitude, 'Longitude': mapboxOptions.origin.longitude},
+      'destination': {
+        'Latitude': mapboxOptions.destination.latitude,
+        'Longitude': mapboxOptions.destination.longitude
+      },
+      'origin': {
+        'Latitude': mapboxOptions.origin.latitude,
+        'Longitude': mapboxOptions.origin.longitude
+      },
       'simulateRoute': mapboxOptions.simulateRoute,
       'setDestinationWithLongTap': mapboxOptions.setDestinationWithLongTap,
       // 'msg': (mapboxOptions.msg != null) ? mapboxOptions.msg : '',
       'profile': (mapboxOptions.profile != null) ? mapboxOptions.profile : '',
       'style': (mapboxOptions.style != null) ? mapboxOptions.style : '',
-      'voiceUnits': (mapboxOptions.voiceUnits != null) ? mapboxOptions.voiceUnits : '',
-      'language': (mapboxOptions.language != null) ? mapboxOptions.language : '',
+      'voiceUnits':
+          (mapboxOptions.voiceUnits != null) ? mapboxOptions.voiceUnits : '',
+      'language':
+          (mapboxOptions.language != null) ? mapboxOptions.language : '',
       // 'alternativeRoute': (mapboxOptions.alternativeRoute != null) ? mapboxOptions.alternativeRoute : false,
     };
     // This is used in the platform side to register the view.
